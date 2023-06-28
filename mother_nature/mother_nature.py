f2a413484caf7467b3993e05f45329e2dab44b5d
Sevie <sevie5@hotmail.com>
2023-06-28 13:56:43 -0400

    create_github_issue fix

--- mother_nature/mother_nature.py
+++ mother_nature/mother_nature.py
@@ -73,12 +73,12 @@ async def get_history(ctx, year: int, month: int, day: int, include_mother_natur
     for w in word_tokens:
       if w not in stop_words:
           filtered_sentence.append(w)
-    await channel.send(' '.join(str(x) for x in filtered_sentence))
+    await channel.respond(' '.join(str(x) for x in filtered_sentence))
 
 @bot.command(name='commands', description='prints all commands', guild=discord.Object(id=996592811887579317))
 async def get_commands(ctx):
   for command in list_commands:
-      await ctx.channel.send(command)    
+      await ctx.channel.respond(command)    
   
 @bot.command(name='is_color_legal', description='checks if a food coloring is legal according to the FDA', guild=discord.Object(id=996592811887579317))
 async def color_legal(ctx):
@@ -86,7 +86,7 @@ async def color_legal(ctx):
     bot.get_fda_reported_lists()
     bot.get_global_chem_lists()
     response = bot.check_list_status()
-    await ctx.channel.send(response)
+    await ctx.channel.respond(response)
 
 @bot.command(name='make_github_issue', description='creates a github issue', guild=discord.Object(id=996592811887579317))
 async def github_issue(ctx, message: str):
@@ -105,12 +105,12 @@ async def github_issue(ctx, message: str):
           if user_role == "Arbiter of Nature":
             await create_issue(ctx.channel, keyword=keyword)
           else:
-            await ctx.channel.send("You do not have the requisite permissions. You must have the role 'Arbiter of Nature' for the categories 'war' and 'narcotics'.")
+            await ctx.channel.respond("You do not have the requisite permissions. You must have the role 'Arbiter of Nature' for the categories 'war' and 'narcotics'.")
         else:
           await create_issue(ctx.channel, keyword=keyword)
 
 async def create_issue(channel, keyword):
-  await channel.send('Creating %s Chemicals Now...' % keyword)
+  await channel.respond('Creating %s Chemicals Now...' % keyword)
 
   keyword = '_'.join(keyword.split())
 
