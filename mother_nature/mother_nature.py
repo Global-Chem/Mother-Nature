c1123277207eec63c94c40507c53f1dfde562b36
solarwaffles <107089996+solarwaffles@users.noreply.github.com>
2023-08-18 11:24:32 -0400

    Removed Print Statement
    Signed-off-by: solarwaffles <107089996+solarwaffles@users.noreply.github.com>

--- mother_nature/mother_nature.py
+++ mother_nature/mother_nature.py
@@ -312,7 +312,6 @@ class MotherNatureCommands(object):
       for keyword in self.__category_keywords__:
         if keyword in channel_name:
           label.append(self.repo.get_label("retrain_%s" % keyword))  
-      print(label)
       self.repo.create_issue(title="SMILE edit Run", labels=label, assignee="Sulstice")
       if retrain_again == False:
         return
